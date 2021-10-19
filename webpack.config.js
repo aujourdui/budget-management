5ab1b818af717cd2e2aad1a5bdeaf89757a52cdf
kyosuke ito <kyosuke.ito@gmail.com>
2021-10-19 14:20:21 -0700

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,57 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-source-map',
-  devServer: {
-    static:
-    {
-      directory: path.join(__dirname, 'public'),
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
     },
-      historyApiFallback: true
-  }
+    performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        })
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-cheap-module-source-map',
+    devServer: {
+      static:
+      {
+        directory: path.join(__dirname, 'public'),
+      },
+        historyApiFallback: true
+    }
+  };
 };
